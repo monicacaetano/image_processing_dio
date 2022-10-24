5cf327f270577623254164a515a67170d3afee40
monicacaetano <monica02caetano@gmail.com>
2022-10-24 15:04:23 -0300

    final version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="image_processing_mmc",
-    version="0.0.2",
+    version="0.0.3",
     author="monicacaetano",
     author_email="monica02caetano@gmail.com",
     description="Image Processing Package using Skimage",
